9abadbcbc46084f72978b2e0d7676a03ce8fbded
Offonika <arseniikeshtov@gmail.com>
2025-07-21 07:59:15 -0500

    Use enums in models

--- app/models/payment.py
+++ app/models/payment.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Enum
 from app.models.base import Base
 from datetime import datetime
 
@@ -9,5 +9,8 @@ class Payment(Base):
     user_id = Column(Integer, nullable=False)
     amount = Column(Integer)
     source = Column(String)
-    status = Column(String)
+    status = Column(
+        Enum("success", "fail", "cancel", "bank_error", name="payment_status"),
+        nullable=False,
+    )
     created_at = Column(DateTime, default=datetime.utcnow)
