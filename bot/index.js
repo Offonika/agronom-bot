de4363647d3b5b2a6425327dd338006511bb142b
Offonika <arseniikeshtov@gmail.com>
2025-07-22 00:27:37 -0500

    [feature] result card and buttons

--- bot/index.js
+++ bot/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const { Pool } = require('pg');
 const { photoHandler, messageHandler, startHandler } = require('./handlers');
+const crypto = require('node:crypto');
 
 const token = process.env.BOT_TOKEN_DEV;
 if (!token) {
@@ -16,6 +17,18 @@ bot.start(startHandler);
 bot.on('photo', (ctx) => photoHandler(pool, ctx));
 
 bot.on('message', messageHandler);
+
+bot.action(/^proto\|/, (ctx) => {
+  const [, product, val, unit, phi] = ctx.callbackQuery.data.split('|');
+  const msg = `Препарат: ${product}\nДоза: ${val} ${unit}\nPHI: ${phi}`;
+  ctx.answerCbQuery();
+  return ctx.reply(msg);
+});
+
+bot.action('ask_expert', (ctx) => {
+  ctx.answerCbQuery();
+  return ctx.reply('Свяжитесь с экспертом для уточнения протокола.');
+});
 
 bot.launch().then(() => console.log('Bot started'));
 
